29e4a8d9b6ec989ba15e0b6f789c90f09f03ba35
BerhanBekele <berhanbzg@gmail.com>
2024-01-17 14:15:11 +0300

    firstCommit

---
+++ JavaTraining/src/main/java/et/com/inhousetrainingL12/models/InventoryManagment.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package et.com.inhousetrainingL12.models;
+
+import et.com.inhousetrainingL12.app.*;
+
+/**
+ *
+ * @author MOD
+ */
+public class InventoryManagment {
+    
+}
---
+++ JavaTraining/src/main/java/et/com/inhousetrainingL12/models/items.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package et.com.inhousetrainingL11.models;
+
+/**
+ *
+ * @author MOD
+ */
+public class items {
+    
+}
